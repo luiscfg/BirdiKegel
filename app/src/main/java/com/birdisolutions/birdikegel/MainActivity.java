26414b54a42fe2ac49b52132c499bb7e9d1bdf2e
luiscarlosfernandez <luiscfg@birdisolutions.com>
2017-05-08 01:17:04 +0200

    Primera versión, pantalla inicio y botones

--- app/src/main/java/com/birdisolutions/birdikegel/MainActivity.java
+++ app/src/main/java/com/birdisolutions/birdikegel/MainActivity.java
@@ -1,7 +1,14 @@
 package com.birdisolutions.birdikegel;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import static android.R.attr.drawable;
+import static android.R.attr.id;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button mconfiguracion;
+
+ // Boton Configuración
+
+        mconfiguracion= (Button)findViewById(R.id.boton_config);
+        mconfiguracion.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                mconfiguracion.setBackgroundResource(R.drawable.ajustes_pulsado); ;
+                //Iniciar Configuración de la aplicación
+                Intent i =new Intent(MainActivity.this, Configuracion.class);
+                startActivity(i);
+            }
+        });
+
+
+
     }
 }
